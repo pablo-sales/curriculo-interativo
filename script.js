2a6ee6ab145be0c25b97e0e8edf338e320397173
pablo-sales <pablo.scsa@gmail.com>
2025-06-25 01:43:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ input.addEventListener('keydown', function(e) {
                 case '1':
                     clearTerminal();
                     response = `
-> SOBRE MIM: Me chamo Pablo, tenho 17 anos, moro em Curitiba no bairro CIC e atualmente sou estudante de Automação Industrial na UTFPR. Sou muito alegre e sempre gosto de ver o lado bom das coisas, gosto muito de passar tempo com minha família e amigos. Sou um entusiasta de jogos e tecnologia e estou sempre buscando aprender mais sobre.  |   Digite '0' para retornar ao menu.
+> SOBRE MIM: Me chamo Pablo, tenho 17 anos, moro em Curitiba no bairro CIC e atualmente sou estudante de Automação Industrial na UTFPR. Sou muito alegre e sempre gosto de ver o lado bom das coisas, gosto muito de passar tempo com minha família e amigos. Sou um entusiasta de jogos e tecnologia e estou sempre buscando aprender mais sobre.  |       Digite '0' para retornar ao menu.
                     `;
                     currentState = 'aba1';
                     break;
@@ -21,7 +21,7 @@ input.addEventListener('keydown', function(e) {
                 case '2':
                     clearTerminal();
                     response = `
-> OBJETIVO: Atuar como aprendiz de manufatura avançada na Bosch para transformar meu entusiasmo por tecnologia em experiêcia prática, contribuindo com muita dedicação e vontade de aprender.   |   Digite '0' para retornar ao menu.
+> OBJETIVO: Atuar como aprendiz de manufatura avançada na Bosch para transformar meu entusiasmo por tecnologia em experiêcia prática, contribuindo com muita dedicação e vontade de aprender.   |       Digite '0' para retornar ao menu.
                     `;
                     currentState = 'aba2';
                     break;
@@ -48,21 +48,74 @@ Digite '0' para retornar ao menu.
                 case '4':
                     clearTerminal();
                     response = `
-> OBJETIVO: 
-Começar a atuar como Aprendiz de Manufatura Avançada na Bosch, para aprender na prática sobre processos industriais inteligentes. 
-
-Digite '0' para retornar ao menu.
+> SOFT SKILLS: Organização | Raciocínio Lógico | Comprometimento com resultados | Trabalho em equipe | Curiosidade/Vontade de aprender | Adaptabilidade     |       Digite '0' para retornar ao menu.
                     `;
                     currentState = 'aba4';
                     break;
     
+                case '5':
+                    clearTerminal();
+                    response = `
+> HARD SKILLS: Nível avançado em Excel | Nível básico em AutoCAD | Lógica de Programação: Tenho compreensão dos princípios fundamentais de algoritmos e estruturas de dados | Linguagens de Programação: Conhecimento básico em C e Python.
+- Inglês nível - A2     |       Digite '0' para retornar ao menu.
+                    `;
+                    currentState = 'aba5';
+                    break;
 
+                case '6':
+                    clearTerminal();
+                    response = `
+> FORMAÇÕES: Atualmente estou cursando Automação Industrial na UTFPR - Noturno | Concluí o Ensino Médio integrado ao Técnico em Administração | Me formei no curso de aprendiz administrativo no CIEE/PR     |      Digite '0' para retornar ao menu.
+                    `;
+                    currentState = 'aba6';
+                    break;                    
+
+                case '7':
+                    clearTerminal();
+                    response = `
+> 
+                    `;
+                    currentState = 'aba7';
+                    break;              
+
+                case '8':
+                    clearTerminal();
+                    response = `
+> HOBBYS E INTERESSES: Tenho como principais hobbies a corrida de rua e os jogos online competitivos. Também aprecio muito momentos em família jogando cartas e jogos de tabuleiro. Tenho grande interesse pelas áreas de engenharia e gosto de estudar temas como história, curiosidades de matemática, elétrica e computação. Sou um entusiasta do mundo automotivo e procuro sempre me manter atualizado sobre novidades e tendências do setor. Além disso, gosto de futebol e sou torcedor apaixonado do São Paulo Futebol Clube    |       Digite '0' para retornar ao menu.
+                    `;
+                    currentState = 'aba8';
+                    break;         
+                    
+                case '9':
+                    clearTerminal();
+                    response = `
+> 
+                    `;
+                    currentState = 'aba9';
+                    break;         
+                    
+                case '10':
+                    clearTerminal();
+                    response = `
+> CONTATO: Número: (41) 98729-8147 | Email: pablo.scsa@gmail.com | LinkedIn: https://www.linkedin.com/in/pablo-schmidt-de-sales-61684528b
+                    `;
+                    currentState = 'aba10';
+                    break;         
+                    
+                    
                 case 'help':
                     response = `
 > COMANDOS DISPONÍVEIS
 1 - Sobre mim
-2 - Habilidades
-3 - Contato
+2 - Objetivo
+3 - Por que a Bosch?
+4 - Soft Skills
+5 - Hard Skills
+6 - Formações
+7 - Linha do Tempo
+8 - Hobbys e Interresses
+9 - Família
+10 - Contato
 
 Digite o número da opção.
                     `;
@@ -102,22 +155,3 @@ function clearTerminal() {
     output.appendChild(header);
 }
 
-
-/*> HABILIDADES
-> Soft Skills
-- Organização
-- Raciocínio Lógico
-- Disciplina
-- Trabalho em equipe
-- Curiosidade
-- Vontade de aprender
-- Adaptabilidade
-
-> Hard Skills
-
-- Conhecimentos básicos de elétrica
-- Nível avançado em Excel
-- Nível intermediário em AutoCAD
-- Lógica de Programação: Tenho compreensão dos princípios fundamentais de algoritmos e estruturas de dados.
-- Linguagens de Programação: Conhecimento básico em C e Python.
-*/
